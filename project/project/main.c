daecb0231708fd9301cb94ecb3df4baa41925a18
yujinlee325 <yu_jin325@naver.com>
2020-10-15 15:33:23 +0900

    1015_1

---
+++ project/project/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  project
+//
+//  Created by 이유진 on 2020/10/15.
+//  Copyright © 2020 이유진. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int flag = 1;
+    int y;
+    while(flag != 0)
+    {
+    
+        y =  3;
+        flag = 0;
+    }
+    y = 4;
+    
+    return 0;
+}
