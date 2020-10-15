69b4c19e136c8f5658d44b61570a1344a1dc4bf1
yujinlee325 <yu_jin325@naver.com>
2020-10-15 16:11:30 +0900

    1015_5

--- project/project/main.c
+++ project/project/main.c
@@ -8,23 +8,24 @@
 
 #include <stdio.h>
 
-void f(void);
-int i;
+void sub(void);
 
 int main(void)
 {
-    for(i=0;i<5;i++)
-    {
-        
-        f();
-    }
+    int i;
+    for (i=0;i<3;i++) {
+        sub();
+}
     return 0;
 }
 
-void f(void)
+void sub(void)
 {
+    int auto_count = 0;
+    static int static_count = 0;
     
-    for(i=0;i<10;i++)
-        printf("#");
-
+    auto_count++;
+    static_count++;
+    printf("auto_count=%d\n", auto_count);
+    printf("static_count=%d\n", static_count);
 }
