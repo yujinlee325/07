7457c59eba26d6494ca399609034954cd1da0f1c
yujinlee325 <yu_jin325@naver.com>
2020-10-15 15:47:19 +0900

    1015_2

--- project/project/main.c
+++ project/project/main.c
@@ -9,15 +9,17 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    int flag = 1;
-    int y;
-    while(flag != 0)
-    {
+
+    int i;
     
-        y =  3;
-        flag = 0;
+    for (i=0;i<5;i++)
+    {
+       int temp = 1;
+        printf("temp = %i\n", temp);
+        temp++;
+        
     }
-    y = 4;
+
     
     return 0;
 }
